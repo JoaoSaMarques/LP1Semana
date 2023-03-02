fc9c568bce3378861145c2705fdeb6336daad862
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-02 22:36:23 +0000

    Exe3Parte1

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        int main()
+        {
+            int number;
+            number = 8
+            Console.WriteLine(number);
+        }
+    }
+}

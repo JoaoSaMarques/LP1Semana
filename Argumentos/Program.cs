a9d84c702cdabf6be02bddbd97a2f2b42590b558
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-02 22:25:15 +0000

    Argumentos

---
+++ Argumentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            args.Length
+            for(int i = 0; i < args.Length; i + 1);
+            {
+                console.WriteLine(args[i]);
+            }
+        }
+
+    }
+}

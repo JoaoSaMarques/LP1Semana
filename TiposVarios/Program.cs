21666f37f92c04381dbecfc4df4ba00c35088a56
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-02 22:40:58 +0000

    Exe3Parte2

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,5 +10,15 @@ namespace TiposVarios
             number = 8
             Console.WriteLine(number);
         }
+        char main()
+        {
+            char newLine1;
+            newLine1 = &#128293;
+            char newLine2;
+            newLine2 = &#129512;
+            char newLine3;
+            newLine3 = &#127879;
+        }
+
     }
 }

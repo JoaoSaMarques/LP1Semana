15861b4718e50943f60b888fcb2f9b8ffe4b99b6
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-02 22:26:51 +0000

    Exe2

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,9 +2,9 @@
 
 namespace OlaMundo
 {
-    class Program
+    //class Program
     {
-        static void Main(string[] args)
+        static void //Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Bye, World!");
